0c5943bfbfe39554eaf4d5c034e7ec48c2832dc3
Ayush Garg <ayush.garg1528@gmail.com>
2026-01-07 23:47:49 +0530

    Contains functions and public APIs

---
+++ Scheduler.h
@@ -0,0 +1,14 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include "process.h"
+
+void FCFS(Process *p, int main);
+void shortestJobFirst(Process *p, int n);
+void ShortestRemainingtimefirst(Process *p, int n);
+void Round_Robin(Process *p, int n, int k);
+void MultiLevelFeedbackQueue(Process *p, int n);
+void LotteryScheduling(Process *p, int n);
+
+
+#endif
